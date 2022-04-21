9d68ca1a70d82cdba5fb17c3cc3495d154bccbcf
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-04-22 01:13:04 +0200

    added all functionality for user + comments

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//instantiate installed modules
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 
+//set port to either predefined server port or 3000 that can be used for local testing
+const port = process.env.PORT || 3000;
+
+//enable app to parse json
 app.use(bodyParser.json());
 
+//array that holds all user data
 let database = [
     {
         id: 0,
@@ -17,177 +22,176 @@ let database = [
         phoneNumber: "06 38719633",
     },
 ];
+//id value will decide user id
 let id = 0;
-
-const users = [
-    {
-        id: 0,
-        firstName: "Mike",
-        lastName: "van der Caaij",
-        street: "Gareelweg 11",
-        isActive: true,
-        emailAddress: "m.vandercaaij@student.avans.nl",
-        password: "secret",
-        phoneNumber: "06 38719633",
-    },
-    {
-        id: 1,
-        firstName: "Grover",
-        lastName: "Armstrong",
-        street: "Angelo Creek",
-        isActive: true,
-        emailAddress: "grover.armstrong@email.com",
-        password: "jB37ohRnwA",
-        phoneNumber: "+385 350.269.2224 x67265",
-    },
-    {
-        id: 2,
-        firstName: "Lane",
-        lastName: "Johns",
-        street: "Annabel Ports",
-        isActive: true,
-        emailAddress: "lane.johns@email.com",
-        password: "ZCIWpBG8oF",
-        phoneNumber: "+56 121-238-1548 x4149",
-    },
-    {
-        id: 3,
-        firstName: "Eddy",
-        lastName: "Raynor",
-        street: "Sipes Ports",
-        isActive: true,
-        emailAddress: "eddy.raynor@email.com",
-        password: "lkquiADLNK",
-        phoneNumber: "+223 (446) 641-7812 x238",
-    },
-    {
-        id: 4,
-        firstName: "Burt",
-        lastName: "White",
-        street: "Buckridge Land",
-        isActive: true,
-        emailAddress: "burt.white@email.com",
-        password: "8MRWaLs6K5",
-        phoneNumber: "+298 330-640-3450",
-    },
-];
 
 //get request on root
 app.get("/", (req, res) => {
+    //show response on home directory
     res.status(200).json({
         code: 200,
-        message: "Hello World",
+        message: 'This is my recreation of the Share-a-Meal api that has been used for the "Programmeren 3" Share-a-Meal application.',
+        course: "Programmeren 4",
+        author: "Mike van der Caaij",
+        studentNumber: 2184147,
     });
 
+    //end response process
     res.end();
 });
 
+//TODO:UC-201
 //UC-201 Register as a new user
 app.post("/user", (req, res) => {
     let user = req.body;
-    console.log(user);
     id++;
     user = {
         id,
         ...user,
     };
-    database.push(user);
-    console.log(database);
-    res.status(201).json({
-        status: 201,
-        result: database,
+
+    //boolean thats used to see if an email is already used
+    let addUser = true;
+
+    //check if given email is already in use
+    database.forEach((el) => {
+        if (el.emailAddress === user.emailAddress) {
+            addUser = false;
+        }
     });
+
+    if (addUser) {
+        //add user if the email is unique
+        database.push(user);
+
+        //return successful status + result
+        res.status(201).json({
+            status: 201,
+            result: database,
+        });
+    } else {
+        //return status + error message
+        res.status(409).json({
+            status: 409,
+            message: `User with the email ${user.emailAddress} already exists.`,
+        });
+    }
 });
 
 //UC-202 Get all users
 app.get("/user", (req, res) => {
-    if (res.statusCode >= 200 && res.statusCode <= 299) {
-        res.status(200).json({
-            status: res.statusCode,
-            result: database,
-        });
-    } else {
-        res.send("Unauthorized. You need to create a new user first, and login, to get a valid JWT.");
-    }
+    //if request if successful return all users
+    res.status(200).json({
+        status: res.statusCode,
+        result: database,
+    });
+
+    //end response process
     res.end();
 });
 
 //UC-203 Request personal user profile
 app.get("/user/profile", (req, res) => {
-    const id = Math.floor(Math.random() * users.length);
+    //On successful get request, display json object showing functionality hasn't been added yet.
+    res.status(200).json({
+        code: 200,
+        message: "This functionality has not been added yet.",
+    });
 
-    if (res.statusCode >= 200 && res.statusCode <= 299) {
-        if (id >= 0 && id <= users.length - 1) {
-            res.send(JSON.stringify(users[id]));
-        } else {
-            res.send("User doesn't exist.");
-        }
-    } else {
-        res.send("Forbidden.");
-    }
+    //end response process
     res.end();
 });
 
 //UC-204 Get single user by ID
 app.get("/user/:id", (req, res) => {
+    //save parameter (id) in variable
     const id = Number(req.params.id);
+
+    //look for user with same id as given in the parameters
     let user = database.filter((item) => item.id === id);
 
+    //check if user is in database
     if (user.length > 0) {
         res.status(200).json({
             status: 200,
             result: user,
         });
     } else {
+        //if the user isn't found return a fitting error response
         res.status(404).json({
             status: 404,
-            result: `User with ID ${id} not found`,
+            message: `User with an id of ${id} doesn't exist`,
         });
     }
+
+    //end response process
     res.end();
 });
 
 //UC-205 Update a single user
 app.put("/user/:id", (req, res) => {
+    //save parameter (id) in variable
     const id = Number(req.params.id);
-    if (res.statusCode >= 200 && res.statusCode <= 299) {
-        if (id >= 0 && id <= users.length) {
-            let user = req.body;
-            user = {
-                id,
-                ...user,
-            };
-            userIndex = database.findIndex((obj) => obj.id === id);
-            database[userIndex] = user;
-            res.send(JSON.stringify(database));
-        } else {
-            res.send("User doesn't exist.");
-        }
-    } else if (res.statusCode === 400) {
-        res.send("Not allowed to edit");
+
+    //set user object with given request body
+    let user = req.body;
+    user = {
+        id,
+        ...user,
+    };
+
+    //filtered array with user that has the same id
+    let existingUser = database.filter((item) => item.id === id);
+
+    //check if there actually is a user in the filtered array
+    if (existingUser.length > 0) {
+        //get index of user in array
+        userIndex = database.findIndex((obj) => obj.id === id);
+        //use index to change object to new object
+        database[userIndex] = user;
+        //return successful status + updated user
+        res.status(201).json({
+            status: 201,
+            message: "User has been updated successfully.",
+            response: user,
+        });
     } else {
-        res.send("Forbidden.");
+        //if the user isn't found return a fitting error response
+        res.status(404).json({
+            status: 404,
+            message: `Can't update user with an id of ${id} because it doesn't exist`,
+        });
     }
+
+    //end response process
     res.end();
 });
 
 //UC-206 Delete a user
 app.delete("/user/:id", (req, res) => {
+    //save parameter (id) in variable
     const id = Number(req.params.id);
 
-    if (res.statusCode >= 200 && res.statusCode <= 299) {
-        if (id >= 0 && id <= users.length - 1) {
-            database = database.filter((item) => item.id !== id);
-            console.log(database);
-            res.send("User deleted!");
-        } else {
-            res.send("User doesn't exist.");
-        }
-    } else if (res.statusCode === 400) {
-        res.send("Not allowed to delete");
+    //look for user with same id as given in the parameters
+    let user = database.filter((item) => item.id === id);
+
+    if (user.length > 0) {
+        //add all users to filtered array except for the user with the same id
+        database = database.filter((item) => item.id !== id);
+        //send successful status
+        res.status(201).json({
+            status: 201,
+            message: "User has been successfully deleted.",
+        });
     } else {
-        res.send("Forbidden.");
+        //if the user isn't found return a fitting error response
+        res.status(404).json({
+            status: 404,
+            message: `Can't delete user with an id of ${id} because it doesn't exist`,
+        });
     }
+
+    //end response process
     res.end();
 });
 
