061446d17e3e28d448558403b048c2888187c912
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-05-01 03:02:35 +0200

    Add meal unit tests

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -10,17 +10,17 @@ exports.validateMeal = (req, res, next) => {
 
     //check if all values are of a certain type
     try {
-        assert(typeof isActive === "boolean" || typeof isActive === "number", "isActive must be a boolean or number.");
-        assert(typeof isVega === "boolean" || typeof isVega === "number", "isVega must be a boolean or number.");
-        assert(typeof isVegan === "boolean" || typeof isVegan === "number", "isVegan must be a boolean or number.");
-        assert(typeof isToTakeHome === "boolean" || typeof isToTakeHome === "number", "isToTakeHome must be a boolean or number.");
-        assert(typeof dateTime === "string", "dateTime must be a string.");
-        assert(typeof maxAmountOfParticipants === "number", "maxAmountOfParticipants must be a number.");
-        assert(typeof price === "number", "price must be a number.");
-        assert(typeof imageUrl === "string", "imageUrl must be a string.");
-        assert(typeof name === "string", "name must be a string.");
-        assert(typeof description === "string", "description must be a string.");
-        assert(typeof allergenes === "object", "allergenes must be an object.");
+        assert(typeof isActive === "boolean" || typeof isActive === "number", "IsActive must be a boolean or number.");
+        assert(typeof isVega === "boolean" || typeof isVega === "number", "IsVega must be a boolean or number.");
+        assert(typeof isVegan === "boolean" || typeof isVegan === "number", "IsVegan must be a boolean or number.");
+        assert(typeof isToTakeHome === "boolean" || typeof isToTakeHome === "number", "IsToTakeHome must be a boolean or number.");
+        assert(typeof dateTime === "string", "DateTime must be a string.");
+        assert(typeof maxAmountOfParticipants === "number", "MaxAmountOfParticipants must be a number.");
+        assert(typeof price === "number", "Price must be a number.");
+        assert(typeof imageUrl === "string", "ImageUrl must be a string.");
+        assert(typeof name === "string", "Name must be a string.");
+        assert(typeof description === "string", "Description must be a string.");
+        assert(typeof allergenes === "object", "Allergenes must be an object.");
 
         return next();
     } catch (err) {
@@ -80,73 +80,6 @@ exports.addMeal = (req, res, next) => {
                 //end response process
                 res.end();
             });
-        });
-    });
-};
-
-//UC-303 Get list of all meals
-exports.getAllMeals = (req, res) => {
-    //create connection to database
-    dbconnection.getConnection((err, connection) => {
-        //throw error if something went wrong
-        if (err) throw err;
-
-        //get all meals
-        connection.query("SELECT * FROM meal", (err, results, fields) => {
-            //throw error if something went wrong
-            if (err) throw err;
-
-            //close connection
-            connection.release();
-
-            //send back all results
-            res.status(200).json({
-                status: 200,
-                result: results,
-            });
-
-            //end response process
-            res.end();
-        });
-    });
-};
-
-//UC-304 Get details of a meal
-exports.getMealByID = (req, res, next) => {
-    dbconnection.getConnection((err, connection) => {
-        //throw error if something went wrong
-        if (err) throw err;
-
-        //save parameter (id) in variable
-        const id = Number(req.params.id);
-
-        //check if parameter is a number
-        if (isNaN(id)) {
-            return next();
-        }
-
-        //get requested meal's data
-        connection.query("SELECT * FROM meal WHERE id = ?", id, (err, results, fields) => {
-            //throw error if something went wrong
-            if (err) throw err;
-
-            //show data if meal exists
-            if (results.length > 0) {
-                //return successful status + result
-                res.status(200).json({
-                    status: 200,
-                    result: results[0],
-                });
-
-                //end response process
-                res.end();
-            } else {
-                //if the meal isn't found return a fitting error response
-                return next({
-                    status: 404,
-                    message: `Meal with an id of ${id} doesn't exist`,
-                });
-            }
         });
     });
 };
@@ -217,6 +150,73 @@ exports.updateMeal = (req, res, next) => {
                 return next({
                     status: 404,
                     message: `Can't update meal with an id of ${id} because it doesn't exist`,
+                });
+            }
+        });
+    });
+};
+
+//UC-303 Get list of all meals
+exports.getAllMeals = (req, res) => {
+    //create connection to database
+    dbconnection.getConnection((err, connection) => {
+        //throw error if something went wrong
+        if (err) throw err;
+
+        //get all meals
+        connection.query("SELECT * FROM meal", (err, results, fields) => {
+            //throw error if something went wrong
+            if (err) throw err;
+
+            //close connection
+            connection.release();
+
+            //send back all results
+            res.status(200).json({
+                status: 200,
+                result: results,
+            });
+
+            //end response process
+            res.end();
+        });
+    });
+};
+
+//UC-304 Get details of a meal
+exports.getMealByID = (req, res, next) => {
+    dbconnection.getConnection((err, connection) => {
+        //throw error if something went wrong
+        if (err) throw err;
+
+        //save parameter (id) in variable
+        const id = Number(req.params.id);
+
+        //check if parameter is a number
+        if (isNaN(id)) {
+            return next();
+        }
+
+        //get requested meal's data
+        connection.query("SELECT * FROM meal WHERE id = ?", id, (err, results, fields) => {
+            //throw error if something went wrong
+            if (err) throw err;
+
+            //show data if meal exists
+            if (results.length > 0) {
+                //return successful status + result
+                res.status(200).json({
+                    status: 200,
+                    result: results[0],
+                });
+
+                //end response process
+                res.end();
+            } else {
+                //if the meal isn't found return a fitting error response
+                return next({
+                    status: 404,
+                    message: `Meal with an id of ${id} doesn't exist`,
                 });
             }
         });
--- test/integration/meal.controller.test.js
+++ test/integration/meal.controller.test.js
@@ -1,128 +1,220 @@
-// const chai = require("chai");
-// const chaiHttp = require("chai-http");
-// const path = require("path");
-// const crypto = require("crypto");
-// const server = require(path.join(__dirname, "../../") + "/index");
-// chai.should();
-// chai.use(chaiHttp);
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const path = require("path");
+const crypto = require("crypto");
+const server = require(path.join(__dirname, "../../") + "/index");
+chai.should();
+chai.use(chaiHttp);
 
-// let deletableUserId;
+let deletableMealId;
 
-// // UC-201 Register as new user
-// describe("UC-201 Register as new user - POST /api/user", () => {
-//     beforeEach((done) => {
-//         done();
-//     });
-//     it("TC-201-1 Required input is missing", (done) => {
-//         chai.request(server)
-//             .post("/api/user")
-//             .send({
-//                 //firstName missing
-//                 lastName: "van der Caaij",
-//                 street: "Gareelweg 11",
-//                 city: "Heerle",
-//                 isActive: true,
-//                 emailAddress: "m.vandercaaij@student.avans.nl",
-//                 password: "dmG!F]!!6cUwK7JQ",
-//                 phoneNumber: "06 38719633",
-//             })
-//             .end((req, res) => {
-//                 res.should.be.an("object");
-//                 let { status, result } = res.body;
-//                 status.should.equals(400);
-//                 result.should.be.a("string").that.equals("First Name must be a string.");
-//                 done();
-//             });
-//     });
-//     it("TC-201-2 Non valid Email Address", (done) => {
-//         chai.request(server)
-//             .post("/api/user")
-//             .send({
-//                 firstName: "Klaas",
-//                 lastName: "Tilburg",
-//                 isActive: true,
-//                 emailAdress: "ts.dsadas",
-//                 password: "dmG!F]!!6cUwK7JQ",
-//                 phoneNumber: "0612345678",
-//                 roles: "editor,guest",
-//                 street: "Hopstraat",
-//                 city: "Amsterdam",
-//             })
-//             .end((req, res) => {
-//                 res.should.be.an("object");
-//                 let { status, result } = res.body;
-//                 status.should.equals(400);
-//                 result.should.be.a("string").that.equals("Email is not valid.");
-//                 done();
-//             });
-//     });
-//     it("TC-201-3 Non valid Password", (done) => {
-//         chai.request(server)
-//             .post("/api/user")
-//             .send({
-//                 firstName: "Klaas",
-//                 lastName: "Tilburg",
-//                 isActive: true,
-//                 emailAdress: "ts.dsadas",
-//                 password: "secret",
-//                 phoneNumber: "0612345678",
-//                 roles: "editor,guest",
-//                 street: "Hopstraat",
-//                 city: "Amsterdam",
-//             })
-//             .end((req, res) => {
-//                 res.should.be.an("object");
-//                 let { status, result } = res.body;
-//                 status.should.equals(400);
-//                 result.should.be.a("string").that.equals("Password's strength is weak. Please fill in a stronger one!");
-//                 done();
-//             });
-//     });
-//     it("TC-201-4 User already exists", (done) => {
-//         chai.request(server)
-//             .post("/api/user")
-//             .send({
-//                 id: 41,
-//                 firstName: "Mike",
-//                 lastName: "van der Caaij",
-//                 isActive: 1,
-//                 emailAdress: "m.vandercaaij@student.nl", //email thats already in use
-//                 password: "dmG!F]!!6cUwK7JQ",
-//                 phoneNumber: "0638719633",
-//                 roles: "editor,guest",
-//                 street: "Gareelwdsadaseg 11",
-//                 city: "Heerle",
-//             })
-//             .end((req, res) => {
-//                 res.should.be.an("object");
-//                 let { status } = res.body;
-//                 status.should.equals(409);
-//                 done();
-//             });
-//     });
-//     it("TC-201-5 User has succesfully been registered", (done) => {
-//         chai.request(server)
-//             .post("/api/user")
-//             .send({
-//                 firstName: "Klaas",
-//                 lastName: "Tilburg",
-//                 isActive: true,
-//                 emailAdress: `${crypto.randomBytes(25).toString("hex")}@gmail.com`,
-//                 password: "dmG!F]!!6cUwK7JQ",
-//                 phoneNumber: "0612345678",
-//                 roles: "editor,guest",
-//                 street: "Hopstraat",
-//                 city: "Amsterdam",
-//             })
-//             .end((req, res) => {
-//                 res.should.be.an("object");
-//                 let { status, result } = res.body;
+// UC-301 Create a meal
+describe("UC-301 Create a meal - POST /api/meal", () => {
+    beforeEach((done) => {
+        done();
+    });
+    it("TC-301-1 Required input is missing", (done) => {
+        chai.request(server)
+            .post("/api/meal")
+            .send({
+                //missing name
+                description: "De pastaklassieker bij uitstek.",
+                isActive: true,
+                isVega: true,
+                isVegan: true,
+                isToTakeHome: true,
+                dateTime: "2022-04-30T23:10:31.775Z",
+                imageUrl: "https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg",
+                allergenes: ["gluten", "noten", "test"],
+                maxAmountOfParticipants: 6,
+                price: 6.75,
+            })
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
+                status.should.equals(400);
+                result.should.be.a("string").that.equals("Name must be a string.");
+                done();
+            });
+    });
+    // it("TC-301-2 Not logged in");
+    it("TC-201-3 Meal succesfully added", (done) => {
+        chai.request(server)
+            .post("/api/meal")
+            .send({
+                name: "Spaghetti Bolognese",
+                description: "De pastaklassieker bij uitstek.",
+                isActive: true,
+                isVega: true,
+                isVegan: true,
+                isToTakeHome: true,
+                dateTime: "2022-04-30T23:10:31.775Z",
+                imageUrl: "https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg",
+                allergenes: ["gluten", "noten", "test"],
+                maxAmountOfParticipants: 6,
+                price: 6.75,
+            })
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
 
-//                 //store id that can be used for the delete test later on
-//                 deletableUserId = result[result.length - 1].id;
+                //store id that can be used for the delete test later on
+                deletableMealId = result[result.length - 1].id;
 
-//                 status.should.equals(201);
-//                 done();
-//             });
-//     });
-// });
+                status.should.equals(201);
+                done();
+            });
+    });
+});
+
+// UC-302 Update meal
+describe("UC-302 Update meal - PUT /api/meal/:id", () => {
+    beforeEach((done) => {
+        done();
+    });
+    it("TC-302-1 Required input is missing", (done) => {
+        chai.request(server)
+            .put("/api/meal/1")
+            .send({
+                //missing name
+                description: "De pastaklassieker bij uitstek.",
+                isActive: true,
+                isVega: true,
+                isVegan: true,
+                isToTakeHome: true,
+                dateTime: "2022-04-30T23:10:31.775Z",
+                imageUrl: "https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg",
+                allergenes: ["gluten", "noten", "test"],
+                maxAmountOfParticipants: 6,
+                price: 6.75,
+            })
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
+                status.should.equals(400);
+                result.should.be.a("string").that.equals("Name must be a string.");
+                done();
+            });
+    });
+    // it("TC-302-2 Not logged in");
+    // it("TC-302-3 Not the owner of this data");
+    it("TC-302-4 Meal doesn't exist", (done) => {
+        chai.request(server)
+            .put("/api/meal/0")
+            .send({
+                name: "Spaghetti Bolognese",
+                description: "De pastaklassieker bij uitstek.",
+                isActive: true,
+                isVega: true,
+                isVegan: true,
+                isToTakeHome: true,
+                dateTime: "2022-04-30T23:10:31.775Z",
+                imageUrl: "https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg",
+                allergenes: ["gluten", "noten", "test"],
+                maxAmountOfParticipants: 6,
+                price: 6.75,
+            })
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status } = res.body;
+                status.should.equals(404);
+                done();
+            });
+    });
+    it("TC-302-5 Meal has been updated successfully", (done) => {
+        chai.request(server)
+            .put("/api/meal/" + deletableMealId)
+            .send({
+                name: "Spaghetti Bolognese",
+                description: "De pastaklassieker bij uitstek.",
+                isActive: true,
+                isVega: true,
+                isVegan: true,
+                isToTakeHome: true,
+                dateTime: "2022-04-30T23:10:31.775Z",
+                imageUrl: "https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg",
+                allergenes: ["gluten", "noten", "test"],
+                maxAmountOfParticipants: 6,
+                price: 6.75,
+            })
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status } = res.body;
+                status.should.equals(201);
+                done();
+            });
+    });
+});
+
+// UC-303 Get list of meals
+describe("UC-303 Get list of meals - GET /api/meal", () => {
+    beforeEach((done) => {
+        done();
+    });
+    it("TC-303-1 Return list of meals", (done) => {
+        chai.request(server)
+            .get("/api/meal")
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
+                status.should.equals(200);
+                done();
+            });
+    });
+});
+
+// UC-304 Get list of meals
+describe("UC-304 Get list of meals - GET /api/meal/:id", () => {
+    beforeEach((done) => {
+        done();
+    });
+    it("TC-304-1 Meal doesn't exist", (done) => {
+        chai.request(server)
+            .get("/api/meal/0")
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
+                status.should.equals(404);
+                done();
+            });
+    });
+    it("TC-304-2 Return details of meal", (done) => {
+        chai.request(server)
+            .get("/api/meal/" + deletableMealId)
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
+                status.should.equals(200);
+                done();
+            });
+    });
+});
+
+// UC-305 Delete meal
+describe("UC-305 Delete meal - DELETE /api/meal/:id", () => {
+    beforeEach((done) => {
+        done();
+    });
+    // it("TC-305-2 Not logged in");
+    // it("TC-305-3 Not the owner of this data");
+    it("TC-305-4 Meal doesn't exist", (done) => {
+        chai.request(server)
+            .delete("/api/meal/0")
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
+                status.should.equals(404);
+                done();
+            });
+    });
+    it("TC-305-5 Meal has been deleted successfully", (done) => {
+        chai.request(server)
+            .delete("/api/meal/" + deletableMealId)
+            .end((req, res) => {
+                res.should.be.an("object");
+                let { status, result } = res.body;
+                status.should.equals(201);
+                done();
+            });
+    });
+});
