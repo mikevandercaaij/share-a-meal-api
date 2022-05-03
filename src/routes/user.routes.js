564a2cc323cf7f0737bb329f5630be2d9a8c9d65
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-05-03 14:46:29 +0200

    heroku push

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
-const userController = require(path.join(__dirname, "../") + "controllers/user.controller");
+// const express = require("express");
+// const router = express.Router();
+// const path = require("path");
+// const userController = require(path.join(__dirname, "../") + "controllers/user.controller");
 
-//add router to exports
-module.exports = router;
+// //add router to exports
+// module.exports = router;
 
-//get request on root
-router.get("/", (req, res) => {
-    //show response on home directory
-    res.status(200).json({
-        code: 200,
-        message: "This is my recreation of the Share-a-Meal api that has been used for our recent Share-a-Meal application.",
-        course: "Programmeren 4",
-        author: "Mike van der Caaij",
-        studentNumber: 2184147,
-    });
+// //get request on root
+// router.get("/", (req, res) => {
+//     //show response on home directory
+//     res.status(200).json({
+//         code: 200,
+//         message: "This is my recreation of the Share-a-Meal api that has been used for our recent Share-a-Meal application.",
+//         course: "Programmeren 4",
+//         author: "Mike van der Caaij",
+//         studentNumber: 2184147,
+//     });
 
-    //end response process
-    res.end();
-});
+//     //end response process
+//     res.end();
+// });
 
-//catch all request for "/api/user/"
-router
-    .route("/api/user")
+// //catch all request for "/api/user/"
+// router
+//     .route("/api/user")
 
-    //UC-201 Register as a new user
-    .post(userController.validateUser, userController.addUser)
+//     //UC-201 Register as a new user
+//     .post(userController.validateUser, userController.addUser)
 
-    //UC-202 Get all users
-    .get(userController.getAllUsers);
+//     //UC-202 Get all users
+//     .get(userController.getAllUsers);
 
-//UC-203 Request personal user profile
-router.get("/api/user/profile", userController.getUserProfile);
+// //UC-203 Request personal user profile
+// router.get("/api/user/profile", userController.getUserProfile);
 
-//catch all request for "/api/user/:id"
-router
-    .route("/api/user/:id")
+// //catch all request for "/api/user/:id"
+// router
+//     .route("/api/user/:id")
 
-    //UC-204 Get single user by ID
-    .get(userController.getUserByID)
+//     //UC-204 Get single user by ID
+//     .get(userController.getUserByID)
 
-    //UC-205 Update a single user
-    .put(userController.validateUser, userController.updateUser)
+//     //UC-205 Update a single user
+//     .put(userController.validateUser, userController.updateUser)
 
-    //UC-206 Delete a user
-    .delete(userController.deleteUser);
+//     //UC-206 Delete a user
+//     .delete(userController.deleteUser);
