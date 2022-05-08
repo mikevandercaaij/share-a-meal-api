4d9c99635f6160af4277a7bcb159bc0a5041ebf0
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-05-08 15:56:21 +0200

    Upgrade mysql version to mysql2

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,8 +1,12 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
     connectionLimit: 10,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    multipleStatements: true,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
